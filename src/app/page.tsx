71a94c9c52bc7feb8f5d44f626d1a20f932958eb
alif mahendra <nacubes20@gmail.com>
2024-10-24 12:48:15 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,8 @@ export default function Home() {
     // }, 100);
 
     try {
-      const lifGPTResponse = await fetch('http://localhost:2000/ai', {
+      const route = process.env.API_ROUTE + "ai"
+      const lifGPTResponse = await fetch(route, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: text })
